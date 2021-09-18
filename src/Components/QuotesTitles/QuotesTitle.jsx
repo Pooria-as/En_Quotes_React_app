f8a75813ec045fdaf43ddfdad6b980e487f46e66
Pooria <p.asiabi@gmail.com>
2021-09-18 22:25:10 +0430

    😁

--- src/Components/QuotesTitles/QuotesTitle.jsx
+++ src/Components/QuotesTitles/QuotesTitle.jsx
@@ -1,12 +1,16 @@
 import React from "react"
 
+
+
 const QuotesTitle = ({title,author}) =>
 {
     return(
 <div>
-<h1>
+            <h1 className='quoteTitle' style={{ 
+              fontSize:'20px'
+             }}>
             {title}
-            </h1>
+              </h1>
             <p>
           {
             author ? 'writer : ' : ''
